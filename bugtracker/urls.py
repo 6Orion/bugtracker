36be239f6314941d51ff6d99c20f6669cfea3250
IP <6orion@gmail.com>
2019-11-10 11:07:53 +0100

    Login URL correction

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView 
 
 from .views import (
                     home_page,
@@ -24,6 +25,7 @@ from .views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', RedirectView.as_view(url='/admin/')),
 
     path('', home_page),
 
