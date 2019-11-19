0e52f6bb37e5dbdb974df29242b19d99ced9c02b
Igor P <6orion@gmail.com>
2019-11-19 16:58:55 +0100

    Adding responsiveness to detail and list views

--- users/forms.py
+++ users/forms.py
@@ -13,4 +13,21 @@ class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'image', 'password')
+        fields = ('password', 'first_name', 'last_name', 'email', 'image')
+
+        labels = {
+            'email': 'E-mail',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'image': 'Avatar image',
+            'password': 'Password',
+            
+        }
+
+        widgets = {
+            'email': forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Your e-mail address'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your name'}),
+            'last_name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your surname'}),
+            'image': forms.ClearableFileInput(attrs={'class':'form-control', 'placeholder':'Your avatar image'}),
+            'password': forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Your password'}),
+        }
